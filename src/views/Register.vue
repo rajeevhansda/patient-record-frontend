968572f49f38a07b7ec235b3d2142ac32e6a06fe
Rajeev Hansda <rajeev@teslon.io>
2023-03-30 12:36:57 +0530

    user register css

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,27 +1,32 @@
 <template>
   <div class="register">
-    <h2>REGISTER</h2>
-    <form class="form" @submit.prevent="registerUser">
+    <div class="container">
+      <h2>REGISTER</h2>
+      <form class="form" @submit.prevent="registerUser">
 
-      <label for="username">Username</label>
-      <input type="text" name="username" autocomplete="on" required="true" />
+        <!-- <label for="username">Username</label> -->
+        <input type="text" name="username" autocomplete="on" required="true" placeholder="Enter Name"/>
 
-      <label for="password">Password</label>
-      <input type="password" name="password" autocomplete="off" required="true" />
+        <!-- <label for="password">Password</label> -->
+        <input type="password" name="password" autocomplete="off" required="true" placeholder="Enter password"/>
 
-      <label for="isAdmin">Is Admin</label>
-      <input type="checkbox" id="isAdmin" name="isAdmin">
+        <div class="label-container">
+
+          <label for="isAdmin">Is Admin</label>
+          <input type="checkbox" id="isAdmin" name="isAdmin">
+        </div>
 
 
-      <button type="submit" class="btn" :disabled="loading">Register</button>
-    </form>
+        <button type="submit" class="btn" :disabled="loading">Register</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
- 
+
   data() {
     return {
       loading: false,
@@ -67,6 +72,92 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.register {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  outline: 1px solid lightgray;
+  border-radius: 5px;
+  width: 80%;
+  max-width: 400px;
+  padding: 20px;
+  align-items: center;
+}
+.container h2{
+  font-weight: bolder;
+  font-size: larger;
+  padding-bottom: 20px;
+}
+
+.container form {
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  width: 80%;
+  margin-bottom: 20px;
+
+}
+
+label {
+  font-weight: bolder;
+  font-size: larger;
+}
+
+input {
+  outline: 1px solid lightgray;
+  width: 100%;
+  margin-bottom: 50px;
+  padding: 5px;
+}
+.label-container{
+  display: flex;
+  flex-direction: column;
+}
+.label-container input{
+  width: max-content;
+  align-items: center;
+  justify-content: center;
+}
+
+
+.btn {
+  background-image: linear-gradient(to right, var(--base-color) 0%, var(--base-dark-color) 51%, var(--base-light-color) 100%);
+  padding: 5px 10px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 5px;
+  display: block;
+}
+
+.submit {
+  width: 100%;
+}
+
+.delete {
+  width: 80%;
+}
+
+.btn:disabled {
+  background-image: none;
+  background-color: gray;
+}
+
+.btn:hover {
+  background-position: right center;
+}
+</style>
 
 
 
